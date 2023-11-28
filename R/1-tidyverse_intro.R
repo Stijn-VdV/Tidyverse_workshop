2cd4dabb3ca865a526dd7f794c9fdf62d6582dc1
Stijn-VdV <vandevondel.stijn@gmail.com>
2023-11-28 10:59:56 +0100

    Import 2022 workshop files

---
+++ R/1-tidyverse_intro.R
@@ -0,0 +1,123 @@
+###############################################
+######## Tidyverse: Data Wrangling 101 ########
+######## **Introduction to Tidyverse** ########
+########_______________________________########
+rm(list = ls())
+.rs.restartR()
+
+# - Install and import tidyverse ----
+# install + load tidyverse
+# if (!require("tidyverse")) install.packages("tidyverse")
+# install.packages("tidyverse")
+
+# check for updates
+tidyverse::tidyverse_update()
+
+# masked functions -> namespaces {package_name}::{function_name}()
+filter
+stats::filter
+dplyr::filter
+
+# - Tibbles ----
+# set seed for reproducibility
+set.seed(1)
+
+# create a dataframe
+a_dataframe <- data.frame(x = 1:25,
+                          y = rnorm(25, 1, 2))
+
+# create a tibble from the dataframe
+# library(tidyverse)
+# as_tibble(a_dataframe)
+a_tibble <- tibble::as_tibble(a_dataframe)
+
+# compare
+a_dataframe
+a_tibble
+
+sum(a_dataframe$y) == sum(a_tibble$y)  # equal sums
+all.equal(a_dataframe, a_tibble)    # some differences (?)
+class(a_dataframe); class(a_tibble) # only differnce is class description
+
+# clean environment
+rm(list = ls())
+
+# - Pipes: basics ----
+# basics
+24 + 9
+sum(24, 9)
+
+# magrittr
+# library(tidyverse)
+# library(dplyr)
+library(magrittr) # pipe from magrittr package: %>%
+24 %>% sum(9)
+24 %>% sum(9, .) # . as placeholder
+
+"one" %>% paste("two")
+"one" %>% paste("two", .)
+
+# base R
+24 |> sum(9)
+
+# show data
+dplyr::glimpse(mtcars)
+str(mtcars)
+head(mtcars)
+
+# load dplyr
+library(dplyr) # filter, mutate, select
+
+  # mtcars #1
+df_cars1 <- tibble::rownames_to_column(mtcars, "car") # function from tibble package
+df_cars1 <- filter(df_cars1, stringr::str_detect(car, "Merc"))
+df_cars1 <- mutate(df_cars1, kml = mpg*(1.60934/3.78541))
+df_cars1 <- select(df_cars1, car, kml, cyl, hp)
+
+  # mtcars #2
+df_cars2 <- select(
+  mutate(
+    filter(tibble::rownames_to_column(mtcars, "car"), stringr::str_detect(car, "Merc")),
+    kml = mpg*(1.60934/3.78541)),
+  car, kml, cyl, hp)
+
+  # mtcars #3
+df_cars3 <- mtcars %>%
+  tibble::rownames_to_column("car") %>%
+  filter(stringr::str_detect(car, "Merc")) %>%
+  # convert miles per gallon -> km per liter
+  mutate(kml = mpg*(1.60934/3.78541)) %>%
+  select(car, kml, cyl, hp)
+
+  # check
+all.equal(df_cars1, df_cars2)
+all.equal(df_cars2, df_cars3)
+all.equal(df_cars1, df_cars3)
+
+# - Tidy data ----
+" - each variable must have its own column
+  - each observation must have its own row
+  - each value must have its own cell
+==> Every variable goes in a column, every observation in a row"
+
+# - Exercises ----
+# EXERCISE 1: Rewrite the following lines of code using %>%
+round(pi, 2)
+sum(1, 5, 20)
+as_tibble(mtcars)
+
+
+# EXERCISE 2: The following block of code is difficult to read because of multiple
+#             layers of nesting. Improve the readability of the code using %>%.
+storms_clean <- summarize(
+  group_by(
+    filter(storms, name == "Sandy"), day),
+  average_wind = mean(wind),
+  sd_wind = sd(wind))
+
+
+# EXERCISE 3: Do the following datasets follow the rules of 'tidy data'?
+#             Why (not)? Use
+mtcars
+relig_income
+billboard
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Tidyverse_workshop
+# Tidyverse workshop 2022
+
+## Target audience
+This course is targeted at Master’s and Ph.D. students with a basic understanding of the R programming language, but want to manipulate (large) datasets with more ease than using spreadsheet software. In other words, being comfortable with basic R operations is required.
+
+## Preparations
+Please go through all of the steps below before attending the workshop!!
+
+Make sure to have at least `R version 4.1.0` installed on your computer. Additionally, I strongly recommend installing RStudio, as I will be using this as my Integrated Development Environment (IDE) throughout this course.
+
+Installing R: https://www.r-project.org/
+Installing RStudio: https://www.rstudio.com/products/rstudio/download/#download
+Finally, install tidyverse using `install.packages("tidyverse")`.
+
+## Prerequisites
+A basic understanding of R is assumed. As we have a lot of ground to cover, it would be unwise to jump in unprepared! If you are not yet comfortable with R basics (loading a package, importing data, creating vectos and dataframes), I strongly recommend the `swirl` package, which interactively introduces you to R (see [swirl’s website](https://swirlstats.com/students.html) for more information). To get started with `swirl` right away, install the package using `install.packages("swirl")`, load it into your session with `library(swirl)` and jump-start your journey with `swirl()`. Going through the first chapter (`1: Basic Building Blocks`) should suffice, but don’t let that stop you from learning moRe!
+
+
